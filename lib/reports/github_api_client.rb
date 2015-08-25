141150343e8fd8f5976655657e0de33b4ea7ceec
Tom <tom_t_lee@yahoo.com>
2015-08-25 15:05:04 +0000

    Add faraday middleware for status code checks

--- lib/reports/github_api_client.rb
+++ lib/reports/github_api_client.rb
@@ -14,17 +14,11 @@ module Reports
   User = Struct.new(:name, :location, :public_repos)
   Repo = Struct.new(:name, :url)
 
-  VALID_STATUS_CODES = [200, 302, 401, 403, 404, 422]
-
   class GitHubAPIClient
     def user_info(username)
       url = "https://api.github.com/users/#{username}"
 
       response = connection.get(url)
-
-      if !VALID_STATUS_CODES.include? response.status
-        raise RequestFailure, JSON.parse(response.body)["message"]
-      end
 
       if response.status == 404
         raise NonexistentUser, "'#{username}' does not exist"
@@ -38,10 +32,6 @@ module Reports
       url = "https://api.github.com/users/#{username}/repos"
 
       response = connection.get(url)
-
-      if !VALID_STATUS_CODES.include? response.status
-        raise RequestFailure, JSON.parse(response.body)["message"]
-      end
 
       if response.status == 404
         raise NonexistentUser, "'#{username}' does not exist"
---
+++ lib/reports/middleware/status_check.rb
@@ -0,0 +1,19 @@
+module Reports
+  module Middleware
+    class StatusCheck < Faraday::Middleware
+      VALID_STATUS_CODES = [200, 302, 401, 403, 404, 422]
+
+      def initialize(app)
+        super(app)
+      end
+
+      def call(env)
+        @app.call.on_complete do |response_env|
+          unless VALID_STATUS_CODES.include? response_env.status
+            raise RequestFailure, JSON.parse(response_env.body)["message"]
+          end
+        end
+      end
+    end
+  end
+end
