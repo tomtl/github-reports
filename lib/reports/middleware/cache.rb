7a127039d6ced0ef5bb807697976ba6acc79302c
Tom <tom_t_lee@yahoo.com>
2015-08-26 18:25:55 +0000

    Add cache control header respecting to caching

--- lib/reports/middleware/cache.rb
+++ lib/reports/middleware/cache.rb
@@ -8,13 +8,31 @@ module Reports
 
       def call(env)
         key = env.url.to_s
-        return @storage[key] if @storage[key]
+        cached_response = @storage[key]
+
+        if cached_response && useable_cache?(cached_response)
+          return cached_response
+        end
 
         response = @app.call(env)
-        response.on_complete do
-          @storage[key] = response if env.method == :get
+        response.on_complete do |response_env|
+          @storage[key] = response if cachable_response?(response_env)
         end
+
         response
+      end
+
+      def useable_cache?(cached_response)
+        cached_response &&
+          !["must-validate", "no-cache", "no-store"].include?(
+            cached_response.headers["Cache-Control"])
+      end
+
+      def cachable_response?(env)
+        env.method == :get &&
+          env.response_headers["Cache-Control"] &&
+          !["no-store", "no-cache"].include?(
+            env.response_headers["Cache-Control"])
       end
     end
   end
