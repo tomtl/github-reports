e62026ba0de8ed28a44a343e7b6be7379e8e1f13
Tom Lee <tom_t_lee@yahoo.com>
2015-09-12 14:11:34 -0400

    Add pagination for user_repos report

--- lib/reports/github_api_client.rb
+++ lib/reports/github_api_client.rb
@@ -43,9 +43,20 @@ module Reports
         raise NonexistentUser, "'#{username}' does not exist"
       end
 
-      data = response.body
+      repos = response.body
 
-      data.map do |repo_data|
+      link_header = response.headers["link"]
+
+      if link_header
+        while match_data = link_header.match(/<(.*)>; rel="next"/)
+          next_page_url = match_data[1]
+          response = client.get(next_page_url)
+          link_header = response.headers["link"]
+          repos += response.body
+        end
+      end
+
+      repos.map do |repo_data|
         Repo.new(repo_data["full_name"], repo_data["url"])
       end
     end
@@ -54,28 +65,24 @@ module Reports
       url = "https://api.github.com/users/#{username}/events/public"
       response = client.get(url)
 
-      if response.status == 404
-        raise NonexistentUser, "'#{username}' does not exist."
-      end
-
       if response.status != 200
         raise NonexistentUser, "'#{username}' does not exist"
       end
 
-      data = response.body
+      events = response.body
 
-      link_header = response.headers['link']
+      link_header = response.headers["link"]
 
       if link_header
         while match_data = link_header.match(/<(.*)>; rel="next"/)
           next_page_url = match_data[1]
           response = client.get(next_page_url)
-          link_header = response.headers['link']
-          data += response.body
+          link_header = response.headers["link"]
+          events += response.body
         end
       end
 
-      data.map do |event_data|
+      events.map do |event_data|
         event_type = event_data["type"]
         repo_name = event_data["repo"]["name"] if event_data["repo"]["name"]
         Event.new(event_type, repo_name)
