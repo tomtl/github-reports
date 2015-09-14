6eab8fa7de246fedc8bd1de53019e5e7f451c291
Tom <tom_t_lee@yahoo.com>
2015-09-14 18:13:03 +0000

    Add simple spec for github_api_client #user_info

--- spec/reports/github_api_client_spec.rb
+++ spec/reports/github_api_client_spec.rb
@@ -1,11 +1,25 @@
-require 'reports/github_api_client'
+require "dotenv"
+require "reports/github_api_client"
+Dotenv.load
 
 module Reports
   RSpec.describe GitHubAPIClient do
+    describe "#user_info" do
+      it "fetches info for a user" do
+        client = GitHubAPIClient.new
 
-    it "can be initialized" do
-      subject
+        user = client.user_info("octocat")
+        expect(user.name).to eq("The Octocat")
+        expect(user.location).to eq("San Francisco")
+        expect(user.public_repos).to eq(5)
+      end
+
+      it "raises an error for a nonexistent user" do
+        client = GitHubAPIClient.new
+        expect(->{
+          client.user_info("nonexistent_username")
+        }).to raise_error(NonexistentUser)
+      end
     end
-
   end
 end
