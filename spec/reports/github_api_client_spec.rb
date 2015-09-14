539e35d3d9a98a4586e156d27015a7e31c53fa8d
Tom <tom_t_lee@yahoo.com>
2015-09-14 18:49:31 +0000

    Add vcr to #user_info specs

--- spec/reports/github_api_client_spec.rb
+++ spec/reports/github_api_client_spec.rb
@@ -1,14 +1,14 @@
-require "dotenv"
+require "vcr_helper"
 require "reports/github_api_client"
-Dotenv.load
 
 module Reports
   RSpec.describe GitHubAPIClient do
     describe "#user_info" do
-      it "fetches info for a user" do
+      it "fetches info for a user", :vcr do
         client = GitHubAPIClient.new
 
         user = client.user_info("octocat")
+
         expect(user.name).to eq("The Octocat")
         expect(user.location).to eq("San Francisco")
         expect(user.public_repos).to eq(5)
@@ -16,6 +16,7 @@ module Reports
 
       it "raises an error for a nonexistent user" do
         client = GitHubAPIClient.new
+
         expect(->{
           client.user_info("nonexistent_username")
         }).to raise_error(NonexistentUser)
